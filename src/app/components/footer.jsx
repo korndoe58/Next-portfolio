d8f212921d990e5bdbf35671f9e5e57dcb1f86ba
korndoe58 <59526316+korndoe58@users.noreply.github.com>
2023-10-03 12:47:41 +0700

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className='h-24 md:h-24 p-5 lg:px-20 xl:px-40 text-black flex items-center justify-between backdrop-blur-sm bg-white/50'>
-        <Link href={'/'}><div className=" h-6 w-6 relative "><Image src={'/logo.png'} alt='logo' sizes="(max-width: 768px) 100vw" fill className=" object-contain" /></div></Link>
+        <Link href={'/'}><div className=" h-6 w-6 relative "><Image src={'/logo.PNG'} alt='logo' sizes="(max-width: 768px) 100vw" fill className=" object-contain" /></div></Link>
         <div className='flex flex-col text-xs'>
             <p className='flex flex-col items-end'>© ALL RIGHTS RESERVED. </p>
             <span>Created by Supakorn Rattnapet</span>
